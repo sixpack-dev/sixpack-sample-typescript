5e0115cb2b379311d060abcf1588b79532ed392c
sixpackbot <system@sixpack.dev>
2026-02-03 13:22:48 +0100

    Working orchestrator, code clean

--- src/example-orchestrator.ts
+++ src/example-orchestrator.ts
@@ -1,5 +1,5 @@
 import { type OrchestratorItem, type OrchestratorContext, s } from 'sixpack-sdk/item'
-import {fileURLToPath} from "node:url";
+import { urlToPath } from "sixpack-sdk/item";
 
 // Define the input schema
 const inputSchema = {
@@ -46,7 +46,7 @@ export async function generate(
 }
 
 export const invoiceOrchestrator: OrchestratorItem = {
-    generatePath: fileURLToPath(import.meta.url), // Path to this file
+    generatePath: urlToPath(import.meta.url), // Path to this file
     metadata: {
         name: 'User with Invoice',
         inputSchema,
